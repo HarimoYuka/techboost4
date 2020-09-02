8d689901dd043c987355dad8d99ebc55f1f6ddae
EC2 Default User <ec2-user@ip-172-31-32-226.us-east-2.compute.internal>
2020-09-02 11:43:50 +0000

    history commit

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\News;
+use App\History;
+use Carbon\carbon;
 
 
 class NewsController extends Controller
@@ -84,8 +86,14 @@ class NewsController extends Controller
       unset($news_form['_token']);
       // 該当するデータを上書きして保存する
       $news->fill($news_form)->save();
-
-      return redirect('admin/news');
+      
+      // 以下を追記
+      $history = new History;
+      $history->news_id = $news->id;
+      $history->edited_at = Carbon::now();
+      $history->save();
+      
+      return redirect('admin/news/');
   }
   
     // 以下を追記　　
--- app/Profile.php
+++ app/Profile.php
@@ -13,4 +13,12 @@ class Profile extends Model
         'hobby' => 'required',
         'introduction' => 'required'
     );
+    
+    // 以下を追記
+    // ProfileNewsモデルに関連付けを行う
+    public function profile_histories()
+    {
+      return $this->hasMany('App\ProfileHistory');
+
+    }
 }
